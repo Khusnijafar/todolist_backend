35618dfffe5a62bdb3f6f606b35094dfe6e4dfca
Khusni Jafar <khusnijafar@gmail.com>
2019-09-17 06:50:36 +0700

    -

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: "remotemysql.com",
+    user: "7iA2d638dk",
+    password: "FoEWl26ht6",
+    database: "7iA2d638dk"
 })
 
 connection.connect((err) => {
