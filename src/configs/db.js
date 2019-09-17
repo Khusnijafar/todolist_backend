3a3f9ca6de349227bae74b1ef2659817dc04c72d
Khusni Jafar <khusnijafar@gmail.com>
2019-09-17 16:22:48 +0700

    fixing

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-    // host: "remotemysql.com",
-    // user: "7iA2d638dk",
-    // password: "FoEWl26ht6",
-    // database: "7iA2d638dk"
-    host: 'localhost',
-    user: "khusni",
-    password: "12345",
-    database: "notedb"
+    host: "remotemysql.com",
+    user: "7iA2d638dk",
+    password: "FoEWl26ht6",
+    database: "7iA2d638dk"
+    // host: 'localhost',
+    // user: "khusni",
+    // password: "12345",
+    // database: "notedb"
 })
 
 connection.connect((err) => {
